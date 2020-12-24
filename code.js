b4bb1071be65467d0673c1b0b2e05d9f1d13f38d
Dmitry Verbovetskyi <verbovetskyi.d@gmail.com>
2020-12-24 10:20:29 +0200

    h/w completed

---
+++ code.js
@@ -0,0 +1,12 @@
+function palindrome(num, step = 0) {
+    const obj = {
+        result: num,
+        step: step
+    };
+    const reversNum = +num.toString().split('').reduce( (a, b) => b + a, '');
+    if (num === reversNum) return obj;
+    return palindrome(num + reversNum, ++step);
+}
+//проверка
+console.log(palindrome(312));
+console.log(palindrome(96));
